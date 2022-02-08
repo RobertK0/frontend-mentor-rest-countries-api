eb605307eb086625d78c713a22f3472796c16af3
Zero0k <robykaurin@gmail.com>
2022-02-08 17:24:06 +0100

    Initial modal functionality added Modal window opens rendering country data upon clicking on a card element

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const cardsContainer = document.querySelector(".card-container");
 const searchBar = document.querySelector(".search-bar");
 const regionSelector = document.querySelector(".region-selector");
+const modal = document.querySelector(".modal");
 
 let cleanDataArr = [];
 const fetchCountries = async function () {
@@ -35,7 +36,7 @@ const fetchCountries = async function () {
 
 const generateMarkup = function (country) {
   return `
-<div class="card">
+<div class="card" data-name="${country.name}">
   <img class="flag" src="${country.flag}" alt="" />
   <div class="text-container">
     <h2>${country.name}</h2>
@@ -52,6 +53,46 @@ const generateMarkup = function (country) {
 </div>
 `;
 };
+
+const generateModalMarkup = function (country) {
+  return `
+<div class="country-container-modal">
+  <img class="flag-modal" src="${country.flag}" alt="" />
+  <div class="text-container-modal">
+    <h2>${country.name}</h2>
+    <div class="text">
+      <strong>Native Name:</strong> <span>${country.nativeName}</span>
+    </div>
+    <div class="text">
+      <strong>Population:</strong> <span>${country.population}</span>
+    </div>
+    <div class="text">
+      <strong>Region:</strong> <span>${country.region}</span>
+    </div>
+    <div class="text">
+      <strong>Subregion:</strong> <span>${country.subregion}</span>
+    </div>
+    <div class="text">
+      <strong>Capital:</strong> <span>${country.capital}</span>
+    </div>
+    <div class="text">
+      <strong>Top Level Domain:</strong> <span>${country.topLevelDomain}</span>
+    </div>
+    <div class="text">
+      <strong>Currencies:</strong> <span>${country.currencies
+        .map((language) => language.name)
+        .join(",")}</span>
+    </div>
+    <div class="text">
+      <strong>Languages:</strong> <span>${country.languages
+        .map((language) => language.name)
+        .join(",")}</span>
+    </div>
+  </div>
+</div>
+`;
+};
+
 const renderCountries = function (countries) {
   cardsContainer.innerHTML = "";
   countries.forEach((country) =>
@@ -68,6 +109,7 @@ const filterResults = function (e) {
   console.log(countriesFiltered);
   renderCountries(countriesFiltered);
 };
+
 const filterRegion = function (e) {
   console.log(e.target.value);
   const countriesFiltered = cleanDataArr.filter((country) =>
@@ -76,7 +118,23 @@ const filterRegion = function (e) {
   renderCountries(countriesFiltered);
 };
 
+const getCountry = function (e) {
+  const card = e.target.closest(".card");
+  if (!card) return;
+  const countryObject = cleanDataArr.filter(
+    (country) => country.name === card.dataset.name
+  );
+  renderModal(countryObject[0]);
+};
+
+const renderModal = function (country) {
+  console.log(country);
+  modal.classList.remove("hidden");
+  modal.insertAdjacentHTML("beforeend", generateModalMarkup(country));
+};
+
 fetchCountries();
 
 searchBar.addEventListener("keyup", filterResults);
 regionSelector.addEventListener("change", filterRegion);
+cardsContainer.addEventListener("click", getCountry);
